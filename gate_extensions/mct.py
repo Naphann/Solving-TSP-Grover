e02c99199773d0b5ab50050542bb85904f33bb66
Naphan Benchasattabuse <naphanben@gmail.com>
2019-11-20 11:33:02 +0900

    fix error checking and reference to glocal variables

--- gate_extensions/mct.py
+++ gate_extensions/mct.py
@@ -197,69 +197,69 @@ def apply_mct_dirty(self, controls, target, ancilla):
     
     
     SRTS(self, controls[-1], anc[-1], target)
-    qc.barrier()
+    self.barrier()
     
     if (n-4)%2 == 0:
         a_idx = 1
         for i in reversed(range(floor((n-4)/2))):
             RT4S(self, anc[a_idx - 1 + i], controls[2*i+3], controls[2*i+4], anc[a_idx + i])
-            qc.barrier()
+            self.barrier()
     else:
         a_idx = 2
         for i in reversed(range(floor((n-4)/2))):
             RT4S(self, anc[a_idx - 1 + i], controls[2*i+4], controls[2*i+5], anc[a_idx + i])
-            qc.barrier()
+            self.barrier()
         RTS(self, anc[0], controls[3], anc[1])
-        qc.barrier()
+        self.barrier()
     
     RT4L(self, controls[0], controls[1], controls[2], anc[0])
-    qc.barrier()
+    self.barrier()
     
     if (n-4)%2 == 0:
         a_idx = 1
         for i in (range(floor((n-4)/2))):
             RT4S_inv(self, anc[a_idx - 1 + i], controls[2*i+3], controls[2*i+4], anc[a_idx + i])
-            qc.barrier()
+            self.barrier()
     else:
         a_idx = 2
         RTS_inv(self, anc[0], controls[3], anc[1])
-        qc.barrier()
+        self.barrier()
         for i in (range(floor((n-4)/2))):
             RT4S_inv(self, anc[a_idx - 1 + i], controls[2*i+4], controls[2*i+5], anc[a_idx + i])
-            qc.barrier()
+            self.barrier()
             
     SRTS_inv(self, controls[-1], anc[-1], target)
-    qc.barrier()
+    self.barrier()
     
     ## SAME AS ABOVE
     if (n-4)%2 == 0:
         a_idx = 1
         for i in reversed(range(floor((n-4)/2))):
             RT4S(self, anc[a_idx - 1 + i], controls[2*i+3], controls[2*i+4], anc[a_idx + i])
-            qc.barrier()
+            self.barrier()
     else:
         a_idx = 2
         for i in reversed(range(floor((n-4)/2))):
             RT4S(self, anc[a_idx - 1 + i], controls[2*i+4], controls[2*i+5], anc[a_idx + i])
-            qc.barrier()
+            self.barrier()
         RTS(self, anc[0], controls[3], anc[1])
-        qc.barrier()
+        self.barrier()
     
     RT4L_inv(self, controls[0], controls[1], controls[2], anc[0])
-    qc.barrier()
+    self.barrier()
     
     if (n-4)%2 == 0:
         a_idx = 1
         for i in (range(floor((n-4)/2))):
             RT4S_inv(self, anc[a_idx - 1 + i], controls[2*i+3], controls[2*i+4], anc[a_idx + i])
-            qc.barrier()
+            self.barrier()
     else:
         a_idx = 2
         RTS_inv(self, anc[0], controls[3], anc[1])
-        qc.barrier()
+        self.barrier()
         for i in (range(floor((n-4)/2))):
             RT4S_inv(self, anc[a_idx - 1 + i], controls[2*i+4], controls[2*i+5], anc[a_idx + i])
-            qc.barrier()
+            self.barrier()
 
 def apply_mct(circuit, controls, target, anc, mode='clean-ancilla'):
     if len(controls) == 1:
